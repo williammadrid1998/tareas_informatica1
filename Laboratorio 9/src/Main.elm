09e9fa95fc2a5ce2a65704326d7a808970eccc64
williammadrid1998 <52182277+williammadrid1998@users.noreply.github.com>
2019-10-08 19:15:57 -0600

    Laboratorio 9

---
+++ Laboratorio 9/src/Main.elm
@@ -0,0 +1,44 @@
+module Main exposing (..)
+
+--Ejercicio #1 
+
+type Grupo a = 
+    Valor a 
+    | Suma (Grupo a) (Grupo a)
+    | Inverso (Grupo a)
+
+
+-- Ejercicio #2 
+
+type Algebra t s = Algebra (t -> s) (s -> s-> s) (s -> s) 
+
+iValor1 b =  b
+iSuma1 a b = a + b
+iInverso1 a = -a
+algebra1 = Algebra iValor1 iSuma1 iInverso1
+
+
+--Ejercicio #3
+
+evaluar : Algebra a s -> Grupo a -> s
+evaluar (Algebra iValor iSuma iInverso ) expr =
+ let 
+    alg = Algebra iValor iSuma iInverso
+ in 
+    case expr of 
+    Valor b -> iValor b 
+    Suma a b -> iSuma (evaluar alg a) (evaluar alg b )
+    Inverso s -> iInverso (evaluar alg s)
+    
+
+--Ejercicio #4
+
+zAlgebra x = 
+   let 
+      iValor s = s  
+      iSuma a b = modBy x (a + b )
+      iInverso y = -x
+   in  
+      Algebra iValor iSuma iInverso  
+
+
